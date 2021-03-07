cb82e701bf9337be6739c2bfb22722be706ff422
Marisa Goode <marisa.k.goode@gmail.com>
2021-03-07 03:44:51 -0500

    added mobile friendliness

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
 // messages to greet a user in the console
-var style1="color:red;font-size:1.5rem;font-weight:bold;";
-var style2="color:cyan;font-size:1.5rem;font-weight:bold;";
+const style1="color:red;font-size:1.5rem;font-weight:bold;";
 console.log("%c" + "IF YOU ARE IN HERE THEN YOU ARE CHEATING!", style1);
+// const style2="color:cyan;font-size:1.5rem;font-weight:bold;";
 // console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style2);
 // console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style2);
 
 // various constants go here
-var theDojo = [ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ];
-var dojoDiv = document.querySelector("#the-dojo");
-var titleDiv = document.querySelector("#title")
-var ninjaCount = 10; // should be a slight challenge
-var unchecked = theDojo.length*theDojo[0].length - ninjaCount;
-                
-// shuffle the dojo
-shuffle2d(theDojo);
+const theDojo = [ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ];
+const titleDiv = document.querySelector("#title")
+const dojoDiv = document.querySelector("#the-dojo");
+let ninjaCount = 10; // should be a slight challenge
+let unchecked = theDojo.length*theDojo[0].length - ninjaCount;
+let inputMode = "CHECK"; 
+let isMobile = true;
 
-// shows the dojo
-// console.table(theDojo);
+// allow the user on mobile to set their mode
+function setMode(element) {
+  let buttons = document.querySelectorAll("#mobile-controls button");
+  buttons.forEach(btn => btn.disabled = false);
+  element.disabled = true;
+  inputMode = element.innerText.toUpperCase();
+}
+
+// helper function to provide button html
+function makeButton(i, j) {
+return `<button class="tatami"` + 
+  `onclick="howMany(${i}, ${j}, event)"` +
+  `onContextMenu="mark(event)"></button>`;
+}
     
 // Creates the rows of buttons for this game
 function render(theDojo) {
-  var result = "";
-  for(var i=0; i<theDojo.length; i++) {
+  let result = "";
+  for(let i=0; i<theDojo.length; i++) {
     result += `<div class="row">`;
-    for(var j=0; j<theDojo[i].length; j++) {
-        result += `<button class="tatami" 
-                    onclick="howMany(${i}, ${j})"
-                    onContextMenu="mark(${i}, ${j}, event)">
-                  </button>`;
+    for(let j=0; j<theDojo[i].length; j++) {
+        result += makeButton(i, j);
     }
     result += "</div>";
   }
   return result;
 }
-    
-// adds the rows of buttons into <div id="the-dojo"></div> 
-dojoDiv.innerHTML = render(theDojo);
 
 // call this function when the game ends
 function gameOver(condition) {
-  var buttons = document.querySelectorAll("#the-dojo button");
-  for(var button of buttons) {
+  let buttons = document.querySelectorAll("#the-dojo button");
+  for(let button of buttons) {
     button.disabled = true;
   }
   switch(condition) {
@@ -66,24 +71,31 @@ function gameOver(condition) {
 
 // helper function to select an element based on it's i and j
 function getElementBy(i, j) {
-  var selector = `.row:nth-child(${i+1}) button:nth-child(${j+1})`;
+  const selector = `.row:nth-child(${i+1}) button:nth-child(${j+1})`;
   return document.querySelector(selector);
 }
 
+// offsets to give us all of the positions around us
+const offsets = [{x: 0, y: -1}, {x: 1, y: -1}, {x: 1, y: 0}, {x: 1, y: 1},
+  {x: 0, y: 1}, {x: -1, y: 1}, {x: -1, y: 0}, {x: -1, y: -1}];
 // function is called when a user left clicks a mat
 // if the mat has a ninja then they lose!
 // if the mat has a 0 then adjacent zeroes are filled
-function howMany(i, j) {
-  var element = getElementBy(i, j);
-  var numNinjas = 0;
-  var offsets = [{x: 0, y: -1}, {x: 1, y: -1}, {x: 1, y: 0}, {x: 1, y: 1},
-    {x: 0, y: 1}, {x: -1, y: 1}, {x: -1, y: 0}, {x: -1, y: -1}];
+function howMany(i, j, event) {
+  console.log(isMobile);
+  console.log(inputMode);
+  if(isMobile && inputMode === "MARK") {
+    return mark(event);
+  }
+  // doing it this way so we can recursively search
+  const element = getElementBy(i, j);
+  let numNinjas = 0;
   if(element.disabled || element.innerText == "X") {
     return; // break case for recursion
   }
-  for(var offset of offsets) {
-    var validInY = i+offset.y > -1 && i+offset.y < theDojo.length;
-    var validInX = j+offset.x > -1 && j+offset.x < theDojo[i].length;
+  for(let offset of offsets) {
+    let validInY = i+offset.y > -1 && i+offset.y < theDojo.length;
+    let validInX = j+offset.x > -1 && j+offset.x < theDojo[i].length;
     if(validInX && validInY) {
       numNinjas += theDojo[i+offset.y][j+offset.x];
     }
@@ -93,9 +105,10 @@ function howMany(i, j) {
   element.blur();
   unchecked--;
   if(numNinjas == 0) {
-    for(var offset of offsets) {
-      var validInY = i+offset.y > -1 && i+offset.y < theDojo.length;
-      var validInX = j+offset.x > -1 && j+offset.x < theDojo[i].length;
+    element.innerText = "";
+    for(let offset of offsets) {
+      let validInY = i+offset.y > -1 && i+offset.y < theDojo.length;
+      let validInX = j+offset.x > -1 && j+offset.x < theDojo[i].length;
       if(validInX && validInY) {
         howMany(i+offset.y, j+offset.x, getElementBy(i+offset.y, j+offset.x));
       }
@@ -118,15 +131,13 @@ function randInt(max) {
 // shuffles the values of a 2d array in place
 // assumes a rectangular playfield at least 1x1
 function shuffle2d(arr2d) {
-  var height = arr2d.length;   // will break if arr2d is null
-  var width = arr2d[0].length; // will break if arr2d is empty
-  for(var i=0; i<height; i++) {
-    for(var j=0; j<width; j++) {
-      var newY = randInt(height);
-      var newX = randInt(width);
-      var temp = arr2d[i][j];
-      arr2d[i][j] = arr2d[newY][newX];
-      arr2d[newY][newX] = temp;
+  const height = arr2d.length;   // will break if arr2d is null
+  const width = arr2d[0].length; // will break if arr2d is empty
+  for(let i=0; i<height; i++) {
+    for(let j=0; j<width; j++) {
+      let y = randInt(height);
+      let x = randInt(width);
+      [arr2d[i][j], arr2d[y][x]] = [arr2d[y][x], arr2d[i][j]];
     }
   }
 }
@@ -134,17 +145,17 @@ function shuffle2d(arr2d) {
 // function is called when the user right clicks
 // the square gains an "X" to indicate a ninja may be hiding there
 // of if it was already marked then unmark it
-function mark(i, j, event) {
-  event.preventDefault(); // want to prevent normal left click behavior
-  // theDojo[i][j] ? console.log("Correct") : console.log("Wrong");
+function mark(event) {
+  event.preventDefault();
   if(!event.target.innerText) {
+    event.target.style.color = "#f00";
     event.target.innerText = "X";
   } else {
+    event.target.style.color = "#000";
     event.target.innerText = "";
     event.target.blur();
   }
 }
-
 
 // the game needed more challenge... double the ninjas!
 function hardMode(ninjaCount = 20) {
@@ -153,8 +164,8 @@ function hardMode(ninjaCount = 20) {
   document.querySelector("#title").classList.add("dark-mode");
   document.querySelector(".hard-mode").disabled = true;
   unchecked = theDojo.length*theDojo[0].length - ninjaCount;
-  for(var i=0; i<theDojo.length; i++) {
-    for(var j=0; j<theDojo[i].length; j++) {
+  for(let i=0; i<theDojo.length; i++) {
+    for(let j=0; j<theDojo[i].length; j++) {
       if(ninjaCount > 0) {
         ninjaCount--;
         theDojo[i][j] = 1;
@@ -166,3 +177,25 @@ function hardMode(ninjaCount = 20) {
   shuffle2d(theDojo);
   dojoDiv.innerHTML = render(theDojo)
 }
+
+// quick and dirty check if on mobile
+function isOnPhone() {
+  if(screen.width > 800) {
+    document.querySelector("#mobile-controls").remove();
+    isMobile = false;
+  } else {
+    document.querySelector("#mobile-controls button").disabled = true;
+  }
+}
+
+// basically starts the whole game
+function gameStart() {
+  isOnPhone();
+  shuffle2d(theDojo);
+  // uncomment to show the ninja locations
+  // console.table(theDojo);
+  // draw the game into index.html
+  dojoDiv.innerHTML = render(theDojo);
+}
+
+gameStart();
